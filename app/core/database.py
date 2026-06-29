741f7e0000e9d1d8fd7aee76ec82f0e49cca9593
H Jivan Singha <hjivansingha123@gmail.com>
2026-06-29 19:32:59 +0530

    phase6 : database.py updated for recycling Connection every 5 mins

--- app/core/database.py
+++ app/core/database.py
@@ -6,9 +6,9 @@ from app.core.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    pool_pre_ping = True
+    pool_pre_ping=True,
+    pool_recycle=300,       # recycle connections every 5 minutes
 )
-
 SessionLocal = sessionmaker(
     autocommit=False ,
     autoflush=False ,
